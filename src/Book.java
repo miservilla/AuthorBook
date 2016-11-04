88d51ae0bba1e13aa3f97eabddb0d5d37060f98c
miservilla <miservilla@gmail.com>
2016-11-04 10:52:05 -0600

    Adding variables, and setter/getter methods for Book class.

--- src/Book.java
+++ src/Book.java
@@ -5,6 +5,12 @@
 //Make helper for valid ISBN length (string.length) of 10 or 13. Call isSame
 // method in Author class.
 public class Book {
+
+    private int pubYear = AuthorBookConstants.UNKNOWN_YEAR;
+    private String title = AuthorBookConstants.UNKNOWN_TITLE;
+    private String isbn = AuthorBookConstants.UNKNOWN_ISBN;
+    private Author author = AuthorBookConstants.UNKNOWN_AUTHOR;
+
     public Book(){
 
     }
@@ -17,8 +23,41 @@ public class Book {
 
     }
 
+    public int getPubYear() {
+        return pubYear;
+    }
+
+    public void setPubYear(int date) {
+        this.pubYear = pubYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
     public static void main(String[] args){
         System.out.println();
 
     }
+
 }
